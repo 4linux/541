df225408f37aaeada15361cd93158eb624a99553
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-11-16 00:29:42 -0300

    add provision

--- terraform/kube-master.tf
+++ terraform/kube-master.tf
@@ -15,12 +15,25 @@ resource "google_compute_instance" "kube_master" {
     access_config{}
   }
 
-#   metadata_startup_script = file("./scripts/provision-cicd-tools.sh")
-
   allow_stopping_for_update = true
   deletion_protection       = false
 
   resource_policies = [google_compute_resource_policy.daily.self_link]
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = self.network_interface.0.access_config.0.nat_ip
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${self.network_interface.0.access_config.0.nat_ip}, --private-key ${local.private_key_path} provision/ansible/kube-master.yaml"
+  }
 
 }
 
